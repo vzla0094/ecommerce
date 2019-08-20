4f6d3c6f6200323331cb9847821df1e1c87f62f2
vzla0094 <vzla0094@gmail.com>
2019-08-20 13:10:11 -0700

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+function HatsPage() {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>;
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h1>404: NOT FOUND</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   );
 }
