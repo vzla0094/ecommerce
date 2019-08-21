68c689b26b636da224975c749c7815255a09c740
vzla0094 <vzla0094@gmail.com>
2019-08-20 17:19:18 -0700

    added header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
-function HatsPage() {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>;
-    </div>
-  );
-}
+import Header from "./components/header/header.component";
 
 function NoMatch() {
   return (
@@ -24,6 +17,7 @@ function NoMatch() {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
